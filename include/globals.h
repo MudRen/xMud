24e7519e558d0780557d8327f7404e0cc2e896f7
oiuv <i@oiuv.cn>
2021-10-20 15:22:51 +0800

    增加兼容性宏定义

--- include/globals.h
+++ include/globals.h
@@ -1,3 +1,8 @@
+#ifndef __SENSIBLE_MODIFIERS__
+#define protected static
+#define nosave static
+#endif
+
 #define LOGIN_OB "/system/object/login"
 #define USER_OB "/system/object/user"
 #define VOID_OB "/system/object/void"
